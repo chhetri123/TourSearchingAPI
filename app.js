8d6dd1a2b3c379712934d718b7732899b8248620
chhetri123 <chhetridon123@gmail.com>
2022-01-31 23:55:24 +0545

    setting up pug

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const path = require("path");
 // modules require
 const tourRoutes = require("./routes/tourRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -15,10 +16,12 @@ const AppError = require("./utils/appError");
 const globalErrorHandler = require("./Controller/errorController");
 
 const app = express();
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
 // Global middleware
 app.use(helmet());
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: "10kb" }));
 
 // data sanitization
@@ -47,7 +50,16 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 100,
   message: "Too many request , please try again after 1 hours",
 });
-// Route Middleware
+
+// Routes for Server-side
+app.get("/", (req, res) => {
+  res.status(200).render("base", {
+    tours: "Adhikhola tour",
+    user: "Manish chhetri",
+  });
+});
+
+// Route Middleware (API routes )
 app.use("/api", limiter);
 app.use("/api/v1/tours", tourRoutes);
 app.use("/api/v1/users", userRoutes);
