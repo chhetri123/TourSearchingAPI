23002b1e18e565376c2e7166f5ae6b1199116902
chhetri123 <chhetridon123@gmail.com>
2022-02-15 18:43:49 +0545

    adding email template

--- utils/email.js
+++ utils/email.js
@@ -1,14 +1,40 @@
 const sgMail = require("@sendgrid/mail");
+const pug = require("pug");
+const { htmlToText } = require("html-to-text");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.url = url;
+    this.firstName = user.name.split(" ")[0];
+    this.from = `Manish Chhetri <${process.env.FROM_EMAIL}>`;
+  }
 
-const sendMail = async (option) => {
-  const msg = {
-    to: process.env.TESTING_EMAIL, // for testing i use my another account
-    from: process.env.EMAIL_USERNAME, //
-    subject: "Password reset token (valid for 10 min)",
-    text: option.message,
-    // html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-  };
-  await sgMail.send(msg);
+  async send(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+    const mailOptions = {
+      to: this.to,
+      from: this.from, //process.env.EMAIL_USERNAME, //
+      subject,
+      html,
+      text: htmlToText(html, { wordwrap: 130 }),
+    };
+    await sgMail.send(mailOptions);
+  }
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the tour family");
+  }
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "Your Password Reset Token valid for only 10 min"
+    );
+  }
 };
-module.exports = sendMail;
