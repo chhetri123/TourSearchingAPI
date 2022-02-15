92f7003b21b1cb45ca873c5be71a1bd56a976f9c
chhetri123 <chhetridon123@gmail.com>
2022-02-15 13:19:20 +0545

    resizing upload image

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,20 +1,12 @@
 const multer = require("multer");
-
+const sharp = require("sharp");
 const User = require("./../model/userModel");
 const { StatusCodes } = require("http-status-codes");
 const factory = require("./handlerController");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/img/users");
-  },
-  filename: (req, file, cb) => {
-    const ext = file.mimetype.split("/")[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  },
-});
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
   if (file.mimetype.startsWith("image")) {
@@ -33,7 +25,21 @@ const upload = multer({
   storage: multerStorage,
   fileFilter: multerFilter,
 });
+
 exports.uploadUserPhoto = upload.single("photo");
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+  next();
+});
 
 const filterObj = (obj, ...filter) => {
   const filteredObj = {};
